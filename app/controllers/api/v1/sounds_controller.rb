e1185fa6cc84fc5ddbf68e38ba0b0e0896209b4f
davidkorff <davidkorff@gmail.com>
2017-02-03 15:15:12 -0500

    fixed sounds controller

--- app/controllers/api/v1/sounds_controller.rb
+++ app/controllers/api/v1/sounds_controller.rb
@@ -1,33 +1,33 @@
 module Api::V1
   class SoundsController < ApplicationController
     def index
-      @sounds = RubberDucky.all
+      @sounds = Sound.all
       render json: @sounds
     end
 
     def show
-      @sound = RubberDucky.find(params[:id])
+      @sound = Sound.find(params[:id])
       render json: @sound
     end
 
     def create
-      @sound = RubberDucky.create()
+      @sound = Sound.create()
       render json: @sound
     end
 
     def create
-      @sound = RubberDucky.create(length: params[:sound][:length], volume: params[:sound][:volume], squeakiness: params[:sound][:squeakiness])
+      @sound = Sound.create(length: params[:sound][:length], volume: params[:sound][:volume], squeakiness: params[:sound][:squeakiness])
       render json: @sound, status: 201
     end
 
     def update
-      @sound =RubberDucky.find(params[:id])
+      @sound =Sound.find(params[:id])
       @sound.update(length: params[:sound][:length], volume: params[:sound][:volume], squeakiness: params[:sound][:squeakiness])
       render json: @sound
     end
 
     def destroy
-      @sound =RubberDucky.find(params[:id])
+      @sound =Sound.find(params[:id])
       @sound.destroy
     end
   end
